b2f320ba2686d1c7d47d33f091e4f6a27f8ea3fa
HaroldZQ <hazq0306@gmail.com>
2021-05-03 00:31:36 -0500

    VARIABLES

--- PRIMER PROGRAMA/src/App.java
+++ PRIMER PROGRAMA/src/App.java
@@ -1,5 +1,15 @@
+//Programa realizado por HaroldZQ
+/*Probando comentario,similar a c o c++*/
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Comenzamos programa de práctica");
+        int i;
+        double m=4.56;
+        boolean uno=false;
+        String Nombre1= "Harold ZAMBRANO";
+        System.out.println(Nombre1);
+        System.out.println(uno);
+        i=(int)m;//(int) sirve para convertir mi variable double(decimal), entero
+        System.out.println(i);
     }
 }
